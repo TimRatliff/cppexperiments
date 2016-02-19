5038e335a0613d25a8fd6c07dfd22576d24c972f
TimothyRatliff <timothykratliff@gmail.com>
2016-02-19 00:13:58 -0600

    helloworld

--- tictactoe/tictactoe/tictactoe.cpp
+++ tictactoe/tictactoe/tictactoe.cpp
@@ -2,10 +2,21 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
 
 int main()
 {
+	int add(int a, int b);
+	using namespace std;
+	cout << "Hello World!" << endl;
+	cout << "Today we are going to add 2 and 4 "<< endl;
+	cout << "2 + 4 = " << add(2, 4) << endl;
+	cout << "Wasn't that easy?" << endl;
+
+
+
+
     return 0;
 }
 
